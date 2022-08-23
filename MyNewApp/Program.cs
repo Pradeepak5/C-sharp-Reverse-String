6bb79c3002a40b319c2621fb34ab2434416170d8
PKumara <PKumara@INCOWN170363>
2022-08-23 17:27:23 +0530

    Add project files.

---
+++ MyNewApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using MyNewApp.Utilities;
+
+namespace MyNewApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter any String to get reverse :");
+            string value = Console.ReadLine();
+            string reversedValue = Utility.Reverse(value);
+            Console.WriteLine($"Secret message: {reversedValue}");
+        }
+    }
+}
+
+namespace MyNewApp.Utilities
+{
+    class Utility
+    {
+        public static string Reverse(string message)
+        {
+            char[] letters = message.ToCharArray();
+            Array.Reverse(letters);
+            return new string(letters);
+        }
+    }
+}
